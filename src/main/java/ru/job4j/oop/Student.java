1628974620c189d7a2a05a03b8129eca19e54ae6
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-14 15:00:47 +0300

    corrected

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -11,9 +11,11 @@ public class Student {
 
     public static void main(String[] args) {
         Student petya = new Student();
-        for (int i = 0; i < 3; i++) {
-            petya.music();
-            petya.song();
-        }
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
     }
 }
