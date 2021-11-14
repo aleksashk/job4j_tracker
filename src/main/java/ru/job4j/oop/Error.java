666a304b3321a607dc52e48ce95d5c7a7e3da6cc
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-14 16:09:01 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("active = " + this.active);
+        System.out.println("status = " + this.status);
+        System.out.println("message = " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 745511, "Error1");
+        Error error2 = new Error(false, -84557, "Error2");
+        Error error3 = new Error(true, 4, "Error3");
+
+        error.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
