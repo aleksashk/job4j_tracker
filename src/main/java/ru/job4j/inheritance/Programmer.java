877d824a59756c9e3ecd652c186757cf164e6775
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-16 16:37:30 +0300

    corrected

--- src/main/java/ru/job4j/inheritance/Programmer.java
+++ src/main/java/ru/job4j/inheritance/Programmer.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class Programmer extends Engineer {
     String language;
+
     public Programmer(String name, String surname, String education, String birthday, String department, String language) {
         super(name, surname, education, birthday, department);
         this.language = language;
