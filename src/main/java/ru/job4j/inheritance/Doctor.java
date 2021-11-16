743f9df81d52aeb19e0b9f2d9d1c9b3edc9ff7f9
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-16 16:32:23 +0300

    corrected

--- src/main/java/ru/job4j/inheritance/Doctor.java
+++ src/main/java/ru/job4j/inheritance/Doctor.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class Doctor extends Profession {
     String type;
+
     public Doctor(String name, String surname, String education, String birthday, String type) {
         super(name, surname, education, birthday);
         this.type = type;
--- src/main/java/ru/job4j/inheritance/Surgeon.java
+++ src/main/java/ru/job4j/inheritance/Surgeon.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class Surgeon extends Doctor {
     String qualification;
+
     public Surgeon(String name, String surname, String education, String birthday, String type, String qualification) {
         super(name, surname, education, birthday, type);
         this.qualification = qualification;
