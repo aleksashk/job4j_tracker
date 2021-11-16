2a2fedac74b2bf73fbb34253521a1b9d82148bb5
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-16 19:49:23 +0300

    corrected

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -3,7 +3,7 @@ package ru.job4j.pojo;
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
         products[index] = null;
-        for (int i = index; i <products.length - 1; i++) {
+        for (int i = index; i < products.length - 1; i++) {
             Product tmp = products[i];
             products[i] = products[i + 1];
             products[i + 1] = tmp;
