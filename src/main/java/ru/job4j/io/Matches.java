3a49464131f06e4017dc62bdbbdf8e4baa20b8c3
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-17 20:18:25 +0300

    corrected

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -12,12 +12,13 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
-            while (matches < 1 || matches > 3) {
+            while ((matches < 1 || matches > 3) && matches <= count) {
                 System.out.println("Вы взяли неправильное количество спичек. Попробуйте ещё раз.");
                 matches = Integer.parseInt(input.nextLine());
             }
             turn = !turn;
             count -= matches;
+            System.out.println("На столе осталось " + count + " спичек");
         }
         String msg = turn ? "Выиграл второй игрок" : "Выиграл первый игрок";
         System.out.println(msg);
