891c2298e854be630f362a0840486e62b6f1e84d
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-17 10:05:06 +0300

    corrected

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -6,20 +6,15 @@ public class Library {
         Book book1 = new Book("Book1", 100);
         Book book2 = new Book("Book2", 500);
         Book book3 = new Book("Book3", 1600);
-
         Book[] library = {cleanCode, book1, book2, book3};
-
         for (int i = 0; i < library.length; i++) {
             System.out.println(library[i]);
         }
-
         library[0] = book3;
         library[3] = cleanCode;
-
         for (Book value : library) {
             System.out.println(value);
         }
-
         for (Book book : library) {
             if (book.getName().equals("Clean code")) {
                 System.out.println(book);
