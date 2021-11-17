aba38ece8fc8ac952a92b36ef865c161df0dfe99
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-17 20:35:15 +0300

    corrected

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,8 +11,7 @@ public class StartUI {
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
             if (select == 0) {
-                System.out.println("=== Create a new Item ===");
-                System.out.print("Enter name: ");
+                System.out.println("=== Create a new Item ===\nEnter name: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
