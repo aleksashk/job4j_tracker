0b47aa81d9d7e178c25061642292c570494f123e
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-18 11:15:40 +0300

    corrected

--- src/main/java/ru/job4j/poly/Aircraft.java
+++ src/main/java/ru/job4j/poly/Aircraft.java
@@ -1,6 +1,6 @@
 package ru.job4j.poly;
 
-public class Aircraft implements Vehicle{
+public class Aircraft implements Vehicle {
     @Override
     public void move() {
         System.out.println("aircraft flies");
