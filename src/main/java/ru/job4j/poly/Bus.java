8fd433636061aeb1f3a27d929b577b97c697a1ed
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-18 10:36:58 +0300

    Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void drive() {
+        System.out.println("Bus is going");
+    }
+
+    @Override
+    public void passengers(int numberOfPassengers) {
+        System.out.println("There are " + numberOfPassengers + " passengers in the bus");
+    }
+
+    @Override
+    public double refuel(int volumeOfFuel) {
+        double price = 2.01;
+        return volumeOfFuel * price;
+    }
+}
