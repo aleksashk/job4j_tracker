903e60d9f979fd466c3b452e80cca09f70592b24
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-19 19:23:21 +0300

    corrected

--- src/main/java/ru/job4j/tracker/FindItemByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindItemByNameAction.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public class FindItemByNameAction implements UserAction{
+public class FindItemByNameAction implements UserAction {
     @Override
     public String name() {
         return "Item found by name";
