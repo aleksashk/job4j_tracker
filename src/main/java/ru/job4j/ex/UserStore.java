6c83c8a0c28e3058de4d97614cc4e45b6ea1b44e
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-21 12:53:41 +0300

    corrected

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -7,12 +7,12 @@ public class UserStore {
                 return user;
             }
         }
-        throw new UserNotFoundException();
+        throw new UserNotFoundException("User not found");
     }
 
     public static boolean validate(User user) throws UserInvalidException {
         if (!user.isValid() || user.getUsername().length() < 3) {
-            throw new UserInvalidException();
+            throw new UserInvalidException("User isn't valid");
         }
         return true;
     }
@@ -26,10 +26,10 @@ public class UserStore {
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
-        } catch (UserInvalidException e) {
-            System.out.println("User isn't valid");
-        } catch (UserNotFoundException e) {
-            System.out.println("User not found");
+        } catch (UserInvalidException uie) {
+            uie.printStackTrace();
+        } catch (UserNotFoundException unfe) {
+            unfe.printStackTrace();
         }
     }
 }
