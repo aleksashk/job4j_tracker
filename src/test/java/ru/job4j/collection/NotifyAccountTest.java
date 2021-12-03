e989ebde0d67eac316e0610eeb18622ca8376f58
Aleksandr <aleksandrphilimonov@gmail.com>
2021-12-03 11:18:27 +0300

    3. Модели данных и HashSet. [#218530] corrected

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.collection;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -17,12 +19,30 @@ public class NotifyAccountTest {
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
-        HashSet<Account> expect = new HashSet<>(
+        HashSet<Account> expected = new HashSet<>(
                 Arrays.asList(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
         );
-        assertThat(NotifyAccount.sent(accounts), is(expect));
+        HashSet<Account> actual = NotifyAccount.sent(accounts);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeDuplicates() {
+        List<Account> list = Arrays.asList(
+                new Account("123456789", "Ivan Ivanov", "0g1h1j2k7i8uu"),
+                new Account("987654321", "Petr Petrov", "122h1j2g9i877"),
+                new Account("123456789", "Ivan Ivanov", "0g1h1j2k7i8uu")
+        );
+        HashSet<Account> actual = NotifyAccount.sent(list);
+        HashSet<Account> expected = new HashSet<>(
+                Arrays.asList(
+                        new Account("987654321", "Petr Petrov", "122h1j2g9i877"),
+                        new Account("123456789", "Ivan Ivanov", "0g1h1j2k7i8uu")
+                        )
+        );
+        Assert.assertEquals(expected, actual);
     }
 }
