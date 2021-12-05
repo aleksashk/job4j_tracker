2e142827f4337e4a415806289450ed8352314b0c
Aleksandr <aleksandrphilimonov@gmail.com>
2021-12-05 12:17:28 +0300

    1. Паспорт и жители [#212656 #235710] corrected

--- src/main/java/ru/job4j/comparator/SortBySetUsingComparator.java
+++ src/main/java/ru/job4j/comparator/SortBySetUsingComparator.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 
 public class SortBySetUsingComparator {
     public static Set<String> sort(List<String> list) {
-        Set<String>resultSet = new TreeSet<>(Comparator.reverseOrder());
+        Set<String> resultSet = new TreeSet<>(Comparator.reverseOrder());
         resultSet.addAll(list);
         return resultSet;
     }
