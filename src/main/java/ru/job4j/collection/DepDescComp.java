ac77804bee97f7733c82b25c2f90597256a91620
Aleksandr <aleksandrphilimonov@gmail.com>
2021-12-16 12:38:55 +0300

    1.1. Анонимные классы [#214142]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -6,7 +6,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] arrayO1 = o1.split("/");
         String[] arrayO2 = o2.split("/");
-        int rsl = arrayO1[0].compareTo(arrayO2[0]);
+        int rsl = arrayO2[0].compareTo(arrayO1[0]);
         return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
