652a3b6aff17200397607b29a407a34ab8485cd2
Aleksandr <aleksandrphilimonov@gmail.com>
2021-12-23 10:44:13 +0300

    6. Функции высшего порядка [#24255]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,8 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -12,12 +11,14 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> testSurname = person -> person.getSurname().equals(key);
+        Predicate<Person> testName = person -> person.getName().equals(key);
+        Predicate<Person> testPhone = person -> person.getPhone().equals(key);
+        Predicate<Person> testAddress = person -> person.getAddress().equals(key);
+        Predicate<Person> combine = testSurname.or(testName.or(testPhone.or(testAddress)));
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getSurname().contains(key)
-            || person.getName().contains(key)
-            || person.getPhone().contains(key)
-            || person.getAddress().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -5,18 +5,10 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class PhoneDictionaryTest {
-    @Test
-    public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
-        phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
-        );
-        ArrayList<Person> persons = phones.find("Petr");
-        String expected = persons.get(0).getSurname();
-        String actual = "Arsentev";
-        Assert.assertEquals(expected, actual);
-    }
 
     @Test
     public void whenDontFind() {
@@ -26,5 +18,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("123");
         Assert.assertTrue(persons.isEmpty());
+    }
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 }
