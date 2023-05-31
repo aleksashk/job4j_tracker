943c621b8cc6fdab10fde64df31e2a9cc8cde99e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-05-31 20:27:56 +0300

    4.0. Что такое Interface. [#504946 # [#504946]]

---
+++ src/main/java/ru/job4j/polymorph/Starter.java
@@ -0,0 +1,11 @@
+package ru.job4j.polymorph;
+
+public class Starter {
+    public static void main(String[] args) {
+        Vehicle townCar = new TownCar();
+        townCar.changeGear();
+        townCar.accelerate();
+        townCar.steer();
+        townCar.brake();
+    }
+}
