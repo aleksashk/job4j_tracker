6c91492abf93da950b36e4f15d50d788f8b57634
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-03 10:29:01 +0300

    4. Принципы раннего возврата и охранных выражений [#504875  [#504875 #420411]] v5

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -28,6 +28,10 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
         if (!hasUpCase) {
             throw new IllegalArgumentException(
