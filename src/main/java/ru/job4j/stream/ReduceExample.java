72207b3300caa0cb045af0361615fbcab0bd95f7
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-13 20:51:50 +0300

    Stream API - reduce() fourth example

--- src/main/java/ru/job4j/stream/ReduceExample.java
+++ src/main/java/ru/job4j/stream/ReduceExample.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,5 +21,25 @@ public class ReduceExample {
                 .reduce(2, (a, b) -> a + b);
         System.out.println(sum1);
 
+        List<Person> people = Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+        int sum3 = people.stream()
+                .reduce(
+                        0,
+                        (a, b) -> {
+                            if (b.getAge() > 25) {
+                                return a + b.getAge();
+                            } else {
+                                return a;
+                            }
+                        },
+                        (a, b) -> a + b
+                );
+        System.out.println(sum3);
     }
 }
