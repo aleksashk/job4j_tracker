4e65077150d6c3c415e7a223d44175183e2fd4b5
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-13 19:43:50 +0300

    Stream API - mapToInt() second example

--- src/main/java/ru/job4j/stream/MapToIntExample.java
+++ src/main/java/ru/job4j/stream/MapToIntExample.java
@@ -10,5 +10,21 @@ public class MapToIntExample {
                 .mapToInt(Integer::parseInt)
                 .filter(e -> e % 2 == 0)
                 .forEach(System.out::println);
+
+        List<Person> people = Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+
+        int sum = people.stream()
+                .filter(e -> e.getAge() > 25)
+                .mapToInt(Person::getAge)
+                .peek(System.out::println)
+                .sum();
+
+        System.out.println("SUMMA: " + sum);
     }
 }
