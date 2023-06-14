db2776f16780539395e43e1d8564c4bc512dfab0
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-06-14 11:25:59 +0300

    Stream API - dropWhile()

--- src/main/java/ru/job4j/stream/TakeWhileExample.java
+++ src/main/java/ru/job4j/stream/TakeWhileExample.java
@@ -1,13 +1,12 @@
 package ru.job4j.stream;
 
-import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class TakeWhileExample {
     public static void main(String[] args) {
-        Stream.of(-25, 100, -8, 0, 1, 2, 3, 4)
-                .sorted(Comparator.naturalOrder())
-                .takeWhile(value -> value < 3)
+        Stream.of(-7, -25, 100, -8, 0, 1, 2, 3, 4)
+//                .sorted(Comparator.naturalOrder())
+                .dropWhile(value -> value < 3)
                 .map(value -> "Result: " + value)
                 .forEach(System.out::println);
     }
